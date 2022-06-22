ef1daf33c8db179b75764e4c5e2212c5d26853f1
Erwan Pelmoine <erwan.pelmoine@erya.io>
2022-06-22 14:41:47 +0100

    fix: improve display of picture

--- src/components/Picture/Picture.component.jsx
+++ src/components/Picture/Picture.component.jsx
@@ -1,6 +1,6 @@
 export const Picture = ({ source, alt }) => {
   return (
-    <div className='flex h-52 items-center  '>
+    <div className='flex h-52 max-w-xs items-center  '>
       <img
         src={source}
         className='mx-auto max-h-52 rounded shadow-xl'
